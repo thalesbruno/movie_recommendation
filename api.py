e45aaeb5cc944eadcc3a5217489c33bd5ede4051
Thales Bruno <thalesbrunom@gmail.com>
2020-10-15 21:16:03 -0300

    update documentation and reduce the list of movies

--- api.py
+++ api.py
@@ -38,6 +38,6 @@ async def read_movie_rec(movie_id: int):
     # Get a list of similar movies in descending order of cosine similarity
     movie_similarity = pd.DataFrame({'cos_similarity': y[movie_id]}).sort_values(
         by='cos_similarity', ascending=False)
-    get_50_i = movie_similarity.head(51).index
+    get_50_i = movie_similarity.head(31).index
     recommendation_list = list(df.iloc[get_50_i]['original_title'].values)
     return {"movie": recommendation_list[0], "recommendation_list": recommendation_list[1:]}
--- recommendation.py
+++ recommendation.py
@@ -4,18 +4,11 @@ import pandas as pd
 import pickle
 
 
-# def get_index_from_title(title: str, df: pd.DataFrame) -> int:
-#     return df[df.title == title].index.values[0]
-
-
-# def get_title_from_index(index: int, df: pd.DataFrame) -> str:
-#     return df[df.index == index]['original_title'].values
-
-
 # Read the data set
 df = pd.read_csv("data/movies_pos.csv")
 
 # Create a count matrix from this new combined column
+# Use TFIDF to evaluate how relevant is each word to the text
 tf = TfidfVectorizer()
 X = tf.fit_transform(df.features)
 
